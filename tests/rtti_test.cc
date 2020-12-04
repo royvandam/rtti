56d6eb3378a377c7f56c5059d9296fd5ae65ae0d
Roy van Dam <royv.dam@gmail.com>
2020-12-04 11:58:57 +0100

    Renamed test class hierarchy

--- tests/rtti_test.cc
+++ tests/rtti_test.cc
@@ -3,68 +3,67 @@
 #include <rtti.hh>
 
 namespace {
+    struct ParentA : RTTI::Base<ParentA> {};
+    struct ChildB : RTTI::Extends<ChildB, ParentA> {};
+    struct ChildA : RTTI::Extends<ChildA, ParentA> {};
 
-    struct Shape : RTTI::Base<Shape> {};
-    struct Square : RTTI::Extends<Square, Shape> {};
-    struct Circle : RTTI::Extends<Circle, Shape> {};
-
-    struct SpecialBase : RTTI::Base<SpecialBase> {};
-    struct SpecialCircle : RTTI::Extends<SpecialCircle, Circle, SpecialBase> {
-        using Shape::cast;
+    struct ExtendedBase : RTTI::Base<ExtendedBase> {};
+    struct ExtendedChildA : RTTI::Extends<ExtendedChildA, ChildA, ExtendedBase> {
+        using ParentA::cast;
     };
 
     class RTTITest : public ::testing::Test {};
 
     TEST_F(RTTITest, TypeIdentification) {
-        Shape shape;
-        EXPECT_EQ(shape.typeId(), RTTI::TypeInfo<Shape>::Id());
-        EXPECT_TRUE(shape.isA(RTTI::TypeInfo<Shape>::Id()));
-        EXPECT_FALSE(shape.isA(RTTI::TypeInfo<int>::Id()));
+        ParentA parent;
+        EXPECT_EQ(parent.typeId(), RTTI::TypeInfo<ParentA>::Id());
+        EXPECT_TRUE(parent.isA(RTTI::TypeInfo<ParentA>::Id()));
+        EXPECT_FALSE(parent.isA(RTTI::TypeInfo<int>::Id()));
 
-        Square square;
-        EXPECT_EQ(square.typeId(), RTTI::TypeInfo<Square>::Id());
-        EXPECT_TRUE(square.isA(RTTI::TypeInfo<Shape>::Id()));
-        EXPECT_TRUE(square.isA(RTTI::TypeInfo<Square>::Id()));
-        EXPECT_FALSE(square.isA(RTTI::TypeInfo<Circle>::Id()));
+        ChildA child_a;
+        EXPECT_EQ(child_a.typeId(), RTTI::TypeInfo<ChildA>::Id());
+        EXPECT_TRUE(child_a.isA(RTTI::TypeInfo<ParentA>::Id()));
+        EXPECT_TRUE(child_a.isA(RTTI::TypeInfo<ChildA>::Id()));
+        EXPECT_FALSE(child_a.isA(RTTI::TypeInfo<ChildB>::Id()));
 
-        Circle circle;
-        EXPECT_EQ(circle.typeId(), RTTI::TypeInfo<Circle>::Id());
-        EXPECT_TRUE(circle.isA(RTTI::TypeInfo<Shape>::Id()));
-        EXPECT_TRUE(circle.isA(RTTI::TypeInfo<Circle>::Id()));
-        EXPECT_FALSE(circle.isA(RTTI::TypeInfo<Square>::Id()));
+        ChildB child_b;
+        EXPECT_EQ(child_b.typeId(), RTTI::TypeInfo<ChildB>::Id());
+        EXPECT_TRUE(child_b.isA(RTTI::TypeInfo<ParentA>::Id()));
+        EXPECT_TRUE(child_b.isA(RTTI::TypeInfo<ChildB>::Id()));
+        EXPECT_FALSE(child_b.isA(RTTI::TypeInfo<ChildA>::Id()));
 
-        SpecialCircle specialCircle;
-        EXPECT_EQ(specialCircle.typeId(), RTTI::TypeInfo<SpecialCircle>::Id());
-        EXPECT_TRUE(specialCircle.isA(RTTI::TypeInfo<Shape>::Id()));
-        EXPECT_TRUE(specialCircle.isA(RTTI::TypeInfo<Circle>::Id()));
-        EXPECT_TRUE(specialCircle.isA(RTTI::TypeInfo<SpecialBase>::Id()));
-        EXPECT_TRUE(specialCircle.isA(RTTI::TypeInfo<SpecialCircle>::Id()));
-        EXPECT_FALSE(specialCircle.isA(RTTI::TypeInfo<Square>::Id()));
+        ExtendedChildA extended_child_a;
+        EXPECT_EQ(extended_child_a.typeId(), RTTI::TypeInfo<ExtendedChildA>::Id());
+        EXPECT_TRUE(extended_child_a.isA(RTTI::TypeInfo<ParentA>::Id()));
+        EXPECT_TRUE(extended_child_a.isA(RTTI::TypeInfo<ChildA>::Id()));
+        EXPECT_TRUE(extended_child_a.isA(RTTI::TypeInfo<ExtendedBase>::Id()));
+        EXPECT_TRUE(extended_child_a.isA(RTTI::TypeInfo<ExtendedChildA>::Id()));
+        EXPECT_FALSE(extended_child_a.isA(RTTI::TypeInfo<ChildB>::Id()));
     }
 
-    TEST_F(RTTITest, Casting) {
-        SpecialCircle circle;
-        EXPECT_EQ(circle.cast<Shape*>(), dynamic_cast<Shape*>(&circle));
-        EXPECT_EQ(circle.cast<Shape*>(), static_cast<Shape*>(&circle));
-        EXPECT_EQ(circle.cast<Circle*>(), &circle);
-        EXPECT_EQ(circle.cast<Square*>(), nullptr);
-        EXPECT_EQ(circle.cast<SpecialBase*>(), dynamic_cast<SpecialBase*>(&circle));
+    TEST_F(RTTITest, DynamicCasting) {
+        ExtendedChildA child_a;
+        EXPECT_EQ(child_a.cast<ParentA*>(), dynamic_cast<ParentA*>(&child_a));
+        EXPECT_EQ(child_a.cast<ParentA*>(), static_cast<ParentA*>(&child_a));
+        EXPECT_EQ(child_a.cast<ChildA*>(), &child_a);
+        EXPECT_EQ(child_a.cast<ChildB*>(), nullptr);
+        EXPECT_EQ(child_a.cast<ExtendedBase*>(), dynamic_cast<ExtendedBase*>(&child_a));
 
-        Shape* shape = circle.cast<Shape*>();
-        EXPECT_TRUE(shape->isA(RTTI::TypeInfo<Circle>::Id()));
-        EXPECT_TRUE(shape->isA(RTTI::TypeInfo<SpecialCircle>::Id()));
-        EXPECT_EQ(shape->cast<Circle*>(), dynamic_cast<Circle*>(shape));
-        EXPECT_EQ(shape->cast<Circle*>(), &circle);
-        EXPECT_EQ(shape->cast<Shape*>(), shape);
-        EXPECT_EQ(shape->cast<Square*>(), nullptr);
+        ParentA* parent = child_a.cast<ParentA*>();
+        EXPECT_TRUE(parent->isA(RTTI::TypeInfo<ChildA>::Id()));
+        EXPECT_TRUE(parent->isA(RTTI::TypeInfo<ExtendedChildA>::Id()));
+        EXPECT_EQ(parent->cast<ChildA*>(), dynamic_cast<ChildA*>(parent));
+        EXPECT_EQ(parent->cast<ChildA*>(), &child_a);
+        EXPECT_EQ(parent->cast<ParentA*>(), parent);
+        EXPECT_EQ(parent->cast<ChildB*>(), nullptr);
 
-        SpecialBase* base = circle.cast<SpecialBase*>();
-        EXPECT_TRUE(base->isA(RTTI::TypeInfo<Shape>::Id()));
-        EXPECT_TRUE(base->isA(RTTI::TypeInfo<Circle>::Id()));
-        EXPECT_TRUE(base->isA(RTTI::TypeInfo<SpecialCircle>::Id()));
-        EXPECT_EQ(base->cast<Circle*>(), dynamic_cast<Circle*>(shape));
-        EXPECT_EQ(base->cast<Circle*>(), &circle);
-        EXPECT_EQ(base->cast<Shape*>(), shape);
-        EXPECT_EQ(base->cast<Square*>(), nullptr);
+        ExtendedBase* base = child_a.cast<ExtendedBase*>();
+        EXPECT_TRUE(base->isA(RTTI::TypeInfo<ParentA>::Id()));
+        EXPECT_TRUE(base->isA(RTTI::TypeInfo<ChildA>::Id()));
+        EXPECT_TRUE(base->isA(RTTI::TypeInfo<ExtendedChildA>::Id()));
+        EXPECT_EQ(base->cast<ChildA*>(), dynamic_cast<ChildA*>(parent));
+        EXPECT_EQ(base->cast<ChildA*>(), &child_a);
+        EXPECT_EQ(base->cast<ParentA*>(), parent);
+        EXPECT_EQ(base->cast<ChildB*>(), nullptr);
     }
 }  // namespace
