d10824cad3777c0f9d4bc287f2b4dd90edadf3d1
Roy van Dam <royv.dam@gmail.com>
2021-08-16 21:49:03 +0200

    Improved benchmark to include both up and down casting

--- benchmark/rtti_benchmark.cc
+++ benchmark/rtti_benchmark.cc
@@ -27,17 +27,24 @@ NativeDynamicCast(benchmark::State& state) {
     for (auto _ : state) {
         Child c;
 
-        GrandParent* g;
-        benchmark::DoNotOptimize(g = dynamic_cast<GrandParent*>(&c));
+        // Upcasting
+        GrandParent* pg;
+        benchmark::DoNotOptimize(pg = dynamic_cast<GrandParent*>(&c));
 
         ParentA* pa;
-        benchmark::DoNotOptimize(pa = dynamic_cast<ParentA*>(g));
+        benchmark::DoNotOptimize(pa = dynamic_cast<ParentA*>(&c));
 
         ParentB* pb;
-        benchmark::DoNotOptimize(pb = dynamic_cast<ParentB*>(g));
+        benchmark::DoNotOptimize(pb = dynamic_cast<ParentB*>(&c));
 
         InvalidCast* invalid;
-        benchmark::DoNotOptimize(invalid = dynamic_cast<InvalidCast*>(g));
+        benchmark::DoNotOptimize(invalid = dynamic_cast<InvalidCast*>(&c));
+
+        // Downcasting
+        Child* pc;
+        benchmark::DoNotOptimize(pc = dynamic_cast<Child*>(pg));
+        benchmark::DoNotOptimize(pc = dynamic_cast<Child*>(pa));
+        benchmark::DoNotOptimize(pc = dynamic_cast<Child*>(pb));
     }
 }
 BENCHMARK(NativeDynamicCast);
@@ -47,17 +54,24 @@ RttiDynamicCast(benchmark::State& state) {
     for (auto _ : state) {
         Child c;
 
-        GrandParent* g;
-        benchmark::DoNotOptimize(g = c.cast<GrandParent>());
+        // Upcasting
+        GrandParent* pg;
+        benchmark::DoNotOptimize(pg = c.cast<GrandParent>());
 
         ParentA* pa;
-        benchmark::DoNotOptimize(pa = g->cast<ParentA>());
+        benchmark::DoNotOptimize(pa = c.cast<ParentA>());
 
         ParentB* pb;
-        benchmark::DoNotOptimize(pb = g->cast<ParentB>());
+        benchmark::DoNotOptimize(pb = c.cast<ParentB>());
 
         InvalidCast* invalid;
-        benchmark::DoNotOptimize(invalid = g->cast<InvalidCast>());
+        benchmark::DoNotOptimize(invalid = c.cast<InvalidCast>());
+
+        // Downcasting
+        Child* pc;
+        benchmark::DoNotOptimize(pc = pg->cast<Child>());
+        benchmark::DoNotOptimize(pc = pa->cast<Child>());
+        benchmark::DoNotOptimize(pc = pb->cast<Child>());
     }
 }
 BENCHMARK(RttiDynamicCast);
